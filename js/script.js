ee43650ef371374f9ce7a21dd9fa4b7508296261
AtiyehHoshanfar <atiyeh.hoshanfar@gmail.com>
2024-06-30 17:02:37 +0330

    feat : hero and flags

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const menuItemUnderline=document.querySelector(".nav-desktop__menu-item__underline");
+const navDesktopMenuItem=document.querySelectorAll(".nav-desktop__menu-item")
+// navDesktopMenuItem.forEach(item=>{
+//     item.addEventListener("click",()=>{
+
+//     })
+// })
