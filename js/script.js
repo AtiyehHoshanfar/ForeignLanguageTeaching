a4c2fe317abb62f79c497c9d29ee9c229191c349
AtiyehHoshanfar <atiyeh.hoshanfar@gmail.com>
2024-07-01 19:21:37 +0330

    feat: popular courses

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
-const menuItemUnderline=document.querySelector(".nav-desktop__menu-item__underline");
-const navDesktopMenuItem=document.querySelectorAll(".nav-desktop__menu-item")
-// navDesktopMenuItem.forEach(item=>{
-//     item.addEventListener("click",()=>{
-
-//     })
-// })
+var swiper = new Swiper(".mySwiper", {
+    autoplay: {
+      delay: 3000,
+      spaceBetween: 40,
+      pauseOnMouseEnter: true
+    },
+    watchSlidesProgress: true,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      480: {
+        slidesPerView: 1,
+      },
+      580: {
+        slidesPerView: 2,
+      },
+      1224: {
+        slidesPerView: 4,
+      }
+    }
+  });
+  
